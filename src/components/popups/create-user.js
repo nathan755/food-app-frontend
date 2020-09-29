e6b56af977fc18d14b60674e32b453d55a786d9a
nathan denholm <nathandenholm@live.co.uk>
2020-09-29 18:09:42 +0100

    add create user pop up (may add a generic form component in future)

--- src/components/popups/create-user.js
+++ src/components/popups/create-user.js
@@ -1,18 +1,73 @@
 import React, { Component } from "react";
 import TextFormField from "../text-field";
+import MenuBox from "../menu-selection";
+import Button from "../button";
 
 class CreateUserPopup extends Component {
     constructor(props){
         super(props)
-
+        
         this.state = {
-
+            disableEmployee:false,
+            disableManager:false,
+            emailErrorMessage:"",
+            passwordErrorMessage:"",
+            confirmPasswordErrorMessage:"",
+            nameErrorMessage:"",
+            role:"",
+            email:"",
+            password:"",
+            confirmPassword:"",
+            name:""
         }
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onRoleClick = this.onRoleClick.bind(this);
+        this.onCreateUserClick = this.onCreateUserClick.bind(this);
+        this.onCancelClick = this.onCancelClick.bind(this);
     }
 
-    onInputChange(){
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.role !== this.state.role){
+            if(this.state.role === "employee"){
+                this.setState({disableManager:true});
+            }
+            else if(this.state.role === "manager"){
+                this.setState({disableEmployee:true});
+            }
+            else {
+                this.setState({
+                    disableEmployee:false,
+                    disableManager:false
+                });
+            }
+        }
+    }
+
+    onInputChange(event){
+        const currentInputValue = event.currentTarget.getAttribute("data-key");
+        this.setState({[currentInputValue]:event.target.value});
+    }
+
+    onRoleClick(event, type){
+        console.log("click",type)
+        if(type === "manager"){
+            this.setState({role:this.state.role==="manager"?"":"manager"})
+        }
+        if(type === "employee"){
+            this.setState({role:this.state.role==="employee"?"":"employee"})
+        }
+    }
+
+    onCreateUserClick(){
+        // make request to db with user data
+    }
+
+    onCancelClick(){
+        // call props cancel function to close pop up 
+    }
+
+    validateFields(){
 
     }
 
@@ -21,7 +76,7 @@ class CreateUserPopup extends Component {
         return(
             <div className="create-user-popup">
                 <TextFormField 
-                    errorMessage={this.state.EmailErrorMessage}
+                    errorMessage={this.state.emailErrorMessage}
                     dataKey="email" 
                     label="Email" 
                     placeholder="Email..."
@@ -41,13 +96,38 @@ class CreateUserPopup extends Component {
                     placeholder="Confirm Password..."
                     onChange={this.onInputChange} 
                 />
-                <TextFormField 
-                    errorMessage={this.state.EmailErrorMessage}
-                    dataKey="email" 
-                    label="Email" 
-                    placeholder="Email..."
+                <label className="box-label">Role</label>
+                <div className="create-user-popup__box-container">
+                    <MenuBox
+                        dataName="employee"
+                        error={this.state.employeeError}
+                        size={"small"}
+                        icon={<i class="fas fa-people-carry"></i>}
+                        title="Employee"
+                        onClick={this.onRoleClick}
+                        disabled={this.state.disableEmployee}
+                    />
+                    <MenuBox
+                        dataName="manager"
+                        size={"small"}
+                        icon={<i class="fas fa-tasks"></i>}
+                        title="Manager"
+                        onClick={this.onRoleClick}
+                        disabled={this.state.disableManager}
+                        error={this.state.managerError}
+                    />
+                </div>
+                  
+                      <TextFormField 
+                    errorMessage={this.state.nameErrorMessage}
+                    dataKey="name" 
+                    label="Full Name" 
+                    placeholder="Full Name..."
                     onChange={this.onInputChange} 
                 />
+                <Button value="Create User" onClick={this.onCreateUserClick}/>
+                <Button value="Cancel" onClick={this.onCancelClick}/>
+
             </div>  
         )
     }
