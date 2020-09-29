76c9193afeaec39da6653ce529d4f3130dd8dab9
nathan denholm <nathandenholm@live.co.uk>
2020-09-29 12:09:59 +0100

    add account management component

---
+++ src/components/account-managment.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import MenuBox from "./menu-selection";
+
+class AccountManagement extends Component {
+    constructor(props){
+        super(props)
+
+
+    }
+
+    render(){
+        console.log("hello")
+        return(
+            <div className="account-management">
+                
+                 
+            </div>
+        )
+    }
+}
+
+export default AccountManagement;
