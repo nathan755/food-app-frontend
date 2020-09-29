57be76adca6b4e0d088334b50b9d3be2fb222b09
nathan denholm <nathandenholm@live.co.uk>
2020-09-29 13:15:01 +0100

    add report component

---
+++ src/components/reports.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Reports extends Component {
+    constructor(props){
+        super(props)
+
+
+    }
+
+    render(){
+        console.log("hello")
+        return(
+            <div className="reports">
+                <h1>Reports</h1>
+                 
+            </div>
+        )
+    }
+}
+
+export default Reports;
