b6a50feefedb4243e2424c8c60500b558d012dfb
nathan denholm <nathandenholm@live.co.uk>
2020-10-01 10:47:16 +0100

    add check on App componentDidMount if user is logged in.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,49 @@ import Login from "./views/login";
 import SignUp from "./views/sign-up";
 import "./sass/core.scss";
 import Dashboard from './views/dashboard';
+import Axios from "axios";
+import {connect} from "react-redux";
+import { logout } from './redux/actions/account';
+import { setAccount } from "./redux/actions/account";
 
 class App extends Component {
+	//	Fix auth later -> 
+	//	https://solidgeargroup.com/refresh-token-autenticacion-jwt-implementacion-nodejs
+	//	Store JTW where ? lol
+	componentDidMount(){
+		// const token = localStorage.getItem(token);
+		// console.log("token",token)
+		
+		if(localStorage.getItem("token")===null){
+			console.log("narp")
+		}
+		else{
+			console.log("yarp")
+			const token = localStorage.getItem("token")
+			const config  = {
+				headers:{"Authorization": `Bearer ${token}`
+			}
+		}
+		Axios.post("http://127.0.0.1:3001/check-login",{}, config)
+				.then((res)=>{
+			console.log("res",res)
+		})
+		.catch((err)=>{
+			console.log("err",err)
+			// if 401:
+				// set redux notification
+				// remove token? 
+				// set redux to logged out ect?
+		})
+	}
+		
+	
+
+		
+		
+
+	}
+
 	render() {
 		return (
 			<Router>
@@ -20,4 +61,11 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+	return{
+		logout:()=>{dispatch(logout())},
+		setAccount:()=>{dispatch(setAccount())}	
+	}
+} 
+
+export default connect(null, mapDispatchToProps)(App);
