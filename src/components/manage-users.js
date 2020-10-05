6b9868e55500d1b9bf187f18e7bfbdc94da11cdf
nathan denholm <nathandenholm@live.co.uk>
2020-10-05 11:41:48 +0100

    manage user component now handles all popups associated with it (not redux)

--- src/components/manage-users.js
+++ src/components/manage-users.js
@@ -6,11 +6,12 @@ import Table from "./table";
 import Axios from "axios";
 import { setNotification } from "../redux/actions/notification";
 import AreYouSure from "./popups/are-you-sure";
+import CreateUser from "./popups/create-user";
 
 class ManageUsers extends Component {
 	constructor(props) {
 		super(props)
-
+		
 		this.state = {
 			createUserPopupOpen: false,
 			inviteUserPopupOpen: false,
@@ -18,9 +19,10 @@ class ManageUsers extends Component {
 			tableConfig: {},
 			loading: true,
 			users: [],
-			confirmDeletePopupVisible: false
+			confirmDeletePopupVisible: false,
+			editUserPopupType:""
 		}
-
+		
 		this.onCreateUserMenuClick = this.onCreateUserMenuClick.bind(this);
 		this.onInviteUserMenuClick = this.onInviteUserMenuClick.bind(this);
 		this.closePopup = this.closePopup.bind(this);
@@ -29,10 +31,46 @@ class ManageUsers extends Component {
 		this.onUpdateUserClick = this.onUpdateUserClick.bind(this);
 		this.onConfirmDeleteNoClick = this.onConfirmDeleteNoClick.bind(this);
 		this.onConfirmDeleteYesClick = this.onConfirmDeleteYesClick.bind(this);
+		
 	}
 	
-	async componentDidMount() {
-		
+	componentDidMount() {
+		this.fetchTabledata();
+	}
+	
+	// -----------> Fetch data functions <-----------
+	async deleteUser(selectedData) {
+		try {
+			const deleteUser = await Axios.delete(`http://127.0.0.1:3001/delete-user?userId=${selectedData.id}`);
+			if (deleteUser.status === 200) {
+				const successDeleteConfig = {
+					title: "User Deleted!",
+					copy: selectedData.firstName + " " + selectedData.lastName + " " + "has been deleted from your account!",
+					type: "success",
+					displayTime: 3000
+				}
+				// fetch updated data
+				this.fetchTabledata();
+				this.props.setNotification(successDeleteConfig);
+				// const sideNav = document.getElementsByClassName("dashboard__side-nav")[0];
+				// sideNav.classList.remove("blur");
+				this.handleBlur("remove")
+				this.setState({ confirmDeletePopupVisible: false });
+			}
+		}
+		catch (error) {
+			const failNotificationConfig = {
+				title: "Error Deleting User",
+				copy: "A network error occured! Unable to delete user",
+				type: "danger",
+				displayTime: 3000
+			}
+			this.props.setNotification(failNotificationConfig);
+		}
+	}
+	
+	async fetchTabledata(){
+		// fetch table data
 		try {
 			const userPromise = await Axios.get(`http://127.0.0.1:3001/account-users?account=${this.props.accountId}`);
 			const users = userPromise.data;
@@ -50,7 +88,7 @@ class ManageUsers extends Component {
 					lastName: user.last_name,
 					email: user.email,
 					role: user.role,
-					id: user.id // look up if i should not use the actual id on the table
+					id: user.id // look up if i should not use the actual id on the table 
 				};
 				const buttons = [
 					{
@@ -88,7 +126,7 @@ class ManageUsers extends Component {
 				],
 				rows: rows
 			}
-			this.setState({ tableConfig, loading: false });
+			this.setState({ tableConfig, loading: false, rows });
 		}
 		catch (error) {
 			const notificationConfig = {
@@ -100,8 +138,9 @@ class ManageUsers extends Component {
 			this.props.setNotification(notificationConfig);
 		}
 	}
-
+	
 	async onDeleteUserClick(event) {
+		// delete buttons handler in table
 		const selectedId = event.currentTarget.getAttribute("data-key");
 		const selectedData = this.state.tableConfig.rows.find(item => parseInt(item.id) === parseInt(selectedId));
 		this.setState({ selectedData: selectedData.data }, async () => {
@@ -111,12 +150,11 @@ class ManageUsers extends Component {
 					this.deleteUser(this.state.selectedData);
 				}
 				else {
-					document.getElementsByClassName("dashboard__side-nav")[0].className += " blur";
+					this.handleBlur()
 					this.setState({ confirmDeletePopupVisible: true });
 				}
 			}
 			catch (error) {
-				console.log("error", error)
 				const failNotificationConfig = {
 					title: "Error Deleting User",
 					copy: "A network error occured! Unable to delete user",
@@ -127,102 +165,111 @@ class ManageUsers extends Component {
 			}
 		});
 	}
+	
+	onEditUserPopupSubmit = () => {
+		// fetch updated data close popup
+		this.handleBlur("remove")
+		this.fetchTabledata();
+		this.setState({createUserPopupOpen:false});
+	}
 
+	// -----------> Click Handlers <-----------
 	onConfirmDeleteYesClick() {
+		// are you sure on yes click handler
 		this.deleteUser(this.state.selectedData);
 	}
-
-	async deleteUser(selectedData) {
-		try {
-			const deleteUser = await Axios.delete(`http://127.0.0.1:3001/delete-user?userId=${selectedData.id}`);
-			console.log("deleteUser",deleteUser)
-			if (deleteUser.status === 200) {
-				const successDeleteConfig = {
-					title: "User Deleted!",
-					copy: selectedData.firstName + " " + selectedData.lastName + " " + "has been deleted from your account!",
-					type: "success",
-					displayTime: 3000
-				}
-				this.props.setNotification(successDeleteConfig);
-				const sideNav = document.getElementsByClassName("dashboard__side-nav")[0];
-				sideNav.classList.remove("blur");
-				this.setState({ confirmDeletePopupVisible: false });
-			}
-		}
-		catch (error) {
-			const failNotificationConfig = {
-				title: "Error Deleting User",
-				copy: "A network error occured! Unable to delete user",
-				type: "danger",
-				displayTime: 3000
-			}
-			this.props.setNotification(failNotificationConfig);
-		}
-	}
-
+	
 	onConfirmDeleteNoClick() {
-		const sideNav = document.getElementsByClassName("dashboard__side-nav")[0];
-		sideNav.classList.remove("blur");
-		this.setState({ confirmDeletePopupVisible: false })
+		// are you sure on no click handler
+		this.handleBlur("remove");
+		this.setState({ confirmDeletePopupVisible: false });
 	}
 
 	onUpdateUserClick(event) {
+		// update buttons handler in table
 		const selectedId = event.currentTarget.getAttribute("data-key");
 		const selectedData = this.state.tableConfig.rows.find(item => parseInt(item.id) === parseInt(selectedId));
-		this.props.setPopup("create-user", selectedData.data);
-		this.setState({ selectedData: selectedData.data });
+		this.handleBlur();
+		this.setState({ selectedData: selectedData.data, userPopupType:"update-user", createUserPopupOpen:true });
 	}
-
-	onCreateUserMenuClick() {
-		this.setState({ createUserPopupOpen: true });
-		this.props.setPopup("create-user");
-	}
-
-	onInviteUserMenuClick() {
-		this.setState({ inviteUserPopupOpen: true });
-		this.props.setPopup("invite-users");
-	}
+	
+	// -----------> Popup handlers <-----------
+	handleBlur = (type) => {
+		if(type === "remove"){
+			const dashboard = document.getElementsByClassName("manage-users")[0];
+			const sideNav = document.getElementsByClassName("dashboard__side-nav")[0];
+			sideNav.classList.remove("blur");
+			dashboard.classList.remove("blur");
+		}
+		else{
+			document.getElementsByClassName("dashboard__side-nav")[0].className += " blur";
+			document.getElementsByClassName("manage-users")[0].className += " blur";
+		}
+	} 
 
 	closePopup() {
+		this.handleBlur("remove");
 		this.setState({
 			createUserPopupOpen: false,
 			inviteUserPopupOpen: false
 		});
 	}
 
+	onCreateUserMenuClick() {
+		this.handleBlur()
+		this.setState({ createUserPopupOpen: true, userPopupType:"create-user" });
+	}
+	
+	onInviteUserMenuClick() {
+		this.setState({ inviteUserPopupOpen: true });
+	}
+	
+	// -----------> Render Functions <-----------
 	renderTable() {
 		if (!this.state.loading) {
 			return (<Table config={this.state.tableConfig} />);
 		}
+		// add loading in future / sorry no data
 		return null;
 	}
-
-
+	
 	render() {
 		return (
-			<div className={`manage-users ${this.state.confirmDeletePopupVisible ?"blur":""}`}>
-				<div className="manage-users__table">
-					<h1>Manage Users</h1>
-					<this.renderTable />
-				</div>
-				<div className="manage-users__buttons">
-					<MenuBox
-						icon={<i class="fas fa-user-plus"></i>}
-						title="Create User"
-						onClick={this.onCreateUserMenuClick}
-					/>
-					<MenuBox
-						icon={<i class="fas fa-envelope"></i>}
-						title="Invite Users"
-						onClick={this.onInviteUserMenuClick}
-					/>
-				</div>
-				
-					{this.state.confirmDeletePopupVisible && <AreYouSure onNoClick={this.onConfirmDeleteNoClick} onYesClick={this.onConfirmDeleteYesClick} />}
-				
-				
-			</div>
-		);
+      <div
+        className={`manage-users`}
+      >
+        <div className="manage-users__table">
+          <h1>Manage Users</h1>
+          <this.renderTable />
+        </div>
+        <div className="manage-users__buttons">
+          <MenuBox
+            icon={<i class="fas fa-user-plus"></i>}
+            title="Create User"
+            onClick={this.onCreateUserMenuClick}
+          />
+          <MenuBox
+            icon={<i class="fas fa-envelope"></i>}
+            title="Invite Users"
+            onClick={this.onInviteUserMenuClick}
+          />
+        </div>
+        {this.state.confirmDeletePopupVisible && (
+          <AreYouSure
+            onNoClick={this.onConfirmDeleteNoClick}
+            onYesClick={this.onConfirmDeleteYesClick}
+          />
+        )}
+        {this.state.createUserPopupOpen && (
+          <CreateUser
+            config={this.state.selectedData}
+            type={this.state.userPopupType}
+            closePopup={this.closePopup}
+            onSubmit={this.onEditUserPopupSubmit}
+          />
+        )}
+      </div>
+    );
 	}
 }
 
